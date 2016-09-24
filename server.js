6fc190ef2f6d9bde9e07071b3b8dcfa9ffdf16bc
dhawalagarwal <dhawal.agarwal12@gmail.com>
2016-09-24 12:50:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.get('/', function (req, res) {
 app.get('/article- one', function(req, res) {
 res.send('article one is requested and will served here');
 });
+app.get('/article- two', function(req, res) {
+res.send('article two is requested and will served here');
+});
+app.get('/article- three', function(req, res) {
+res.send('article three is requested and will served here');
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
